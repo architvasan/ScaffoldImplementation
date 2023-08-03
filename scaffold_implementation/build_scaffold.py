65b068eff6a2ebfda504a9c74c01d062fc250b3a
Archit <akvasan2@illinois.edu>
2023-08-03 04:03:46 +0000

    added directory with code

---
+++ scaffold_implementation/build_scaffold.py
@@ -0,0 +1,18 @@
+# Import scaffoldgraph
+import scaffoldgraph as sg
+
+# Import networkx
+import networkx as nx
+
+# Import plotting tools
+import matplotlib.pyplot as plt
+
+# Import rdkit
+from rdkit.Chem import Draw
+from rdkit import Chem
+
+import random
+import os
+
+
+
